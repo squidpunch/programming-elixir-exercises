65096af0fc7fb522af512c842cf17ca79f005daa
David Larrabee <david.larrabee@meyouhealth.com>
2015-05-08 08:51:03 -0400

    Functions 5 - & Notation

---
+++ chapter5/functions5.exs
@@ -0,0 +1,7 @@
+# Functions - Exercise 5
+# Use `&` notation to rewrite the following
+# Enum.map [1,2,3,4], fn x -> x + 2 end
+# Enum.each [1,2,3,4], fn x -> IO.inspect x end
+
+Enum.map [1,2,3,4], &(&1 + 2)
+Enum.each [1,2,3,4], &IO.inspect(&1)
