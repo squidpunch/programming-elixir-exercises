c8d51ed5b2a363d4affa43605ec541b06a164fd7
David Larrabee <david.larrabee@meyouhealth.com>
2015-05-31 19:05:29 -0400

    Sum without an accumulator.

--- chapter7/lists/sum.exs
+++ chapter7/lists/sum.exs
@@ -1,6 +1,4 @@
 defmodule MyList do
-  def sum(list), do: _sum(list, 0)
-
-  defp _sum([], total), do: total
-  defp _sum([ head | tail ], total), do: _sum(tail, head+total)
+  def sum([]), do: 0
+  def sum([ head | tail ]), do: head + sum(tail)
 end
