a7f1444cc3e5ddfdfd02cfa8965e46f36afcf4d2
David Larrabee <david.larrabee@meyouhealth.com>
2015-05-31 19:32:45 -0400

    ListsAndRecursion-1
    Write a mapsum function that takes a list and a function. It applies
    the function to each element of the list and then sums the result.
    
    example: MyList.mapsum [1,2,3], &(&1*&1)
    14

--- chapter7/lists/sum.exs
+++ chapter7/lists/sum.exs
@@ -1,4 +1,7 @@
 defmodule MyList do
+  def mapsum([], _func), do: 0
+  def mapsum([ head | tail], func), do: func.(head) + mapsum(tail, func)
+
   def sum([]), do: 0
   def sum([ head | tail ]), do: head + sum(tail)
 end
